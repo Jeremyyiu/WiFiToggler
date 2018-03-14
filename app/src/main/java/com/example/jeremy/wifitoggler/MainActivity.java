11d3055cdbbc2a1cc4e125ce306287f70bda6e89
Jeremy Yiu <utsjeremy@gmail.com>
2018-03-14 13:11:50 +1100

    fixed toggle

--- app/src/main/java/com/example/jeremy/wifitoggler/MainActivity.java
+++ app/src/main/java/com/example/jeremy/wifitoggler/MainActivity.java
@@ -2,27 +2,37 @@ package com.example.jeremy.wifitoggler;
 
 import android.content.Context;
 import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
 import android.view.View;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
         SwitchCompat wifiSwitch = (SwitchCompat) findViewById(R.id.wifiSwitch);
         initSwitch(wifiSwitch);
     }
 
     private void initSwitch(SwitchCompat wifiSwitch) {
-        //initialise the switch
-        wifiSwitch.setChecked(wifiStatus());
+        boolean wifiStatus = getWifiState();
+        wifiSwitch.setChecked(wifiStatus);
+
+        wifiSwitch.setOnCheckedChangeListener(new SwitchCompat.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                toggleWifi();
+            }
+        });
 
         wifiSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,14 +42,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private boolean wifiStatus() {
-        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    private boolean getWifiState() {
         return wifiManager.isWifiEnabled();
     }
 
     public void toggleWifi() {
-        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
         boolean currentState = wifiManager.isWifiEnabled();
         wifiManager.setWifiEnabled(!currentState);
     }
